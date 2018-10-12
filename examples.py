af1f3c829846f205e6c225673a05c864b3fccdad
何恺一 <hekaiyi@hekaiyideMacBook-Air.local>
2018-10-12 16:34:52 +0300

    fixes in Datasetter

--- examples.py
+++ examples.py
@@ -7,6 +7,7 @@ def get_batch(batch_size=10):
 if __name__ == '__main__':
     batch = get_batch(10)
 
+
     module = models.ResNetDepth(num_channels=3,layers=[1,1,1,1],num_elements=160)
     with torch.no_grad():
         module, batch = module.cuda(), batch.cuda()
