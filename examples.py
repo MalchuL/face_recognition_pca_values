303fc3aca2fd9e40787db9f45413e26204a54683
FeeldRuss <vladichion@gmail.com>
2018-10-12 20:35:42 +0300

    pickle

--- examples.py
+++ examples.py
@@ -1,17 +1,27 @@
 import torch
 import models
 import numpy as np
+import pickle
 from sklearn.preprocessing import StandardScaler
 
 def prepare_scaler(name_file, csv_dir):
     tmp_df = np.genfromtxt(name_file, delimiter='\n', dtype=str)
     files = []
     for name in tmp_df:
-        files.append(np.genfromtxt(csv_dir + name + '.csv', delimiter='\n'))
-    scaler =StandardScaler()
+        files.append(np.genfromtxt(csv_dir + name + '.csv', delimiter = '\n'))
+    scaler = StandardScaler()
     scaler.fit(files)
     return scaler
 
+def dump_object(obj, file_name):
+    file = open(file_name, 'wb')
+    pickle.dump(obj, file)
+    file.close()
+
+def restore_object(file_name):
+    file = open(file_name, 'rb')
+    obj = pickle.load(file)
+    file.close()
 
 def get_batch(batch_size=10):
     return torch.randn(batch_size,3, 450, 450),torch.randn(batch_size,199)
