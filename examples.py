3137bc9c0457521af7f2272cad63a4f790866c12
Vlad Sorokin <malchugan666@gmail.com>
2018-10-12 16:13:09 +0300

    little fixes

--- examples.py
+++ examples.py
@@ -2,7 +2,7 @@ import torch
 import models
 
 def get_batch(batch_size=10):
-    return torch.randn(1,3, 450, 450)
+    return torch.randn(batch_size,3, 450, 450)
 
 if __name__ == '__main__':
     batch = get_batch(10)
