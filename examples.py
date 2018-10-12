33ea117bafc1a90a6bd0ab1bca04a492306d1985
FeeldRuss <vladichion@gmail.com>
2018-10-12 17:59:41 +0300

    added scaler

--- examples.py
+++ examples.py
@@ -1,13 +1,25 @@
 import torch
 import models
+import numpy as np
+from sklearn.preprocessing import StandardScaler
+
+def prepare_scaler(name_file, csv_dir):
+    tmp_df = np.genfromtxt(name_file, delimiter='\n', dtype=str)
+    files = []
+    for name in tmp_df:
+        files.append(np.genfromtxt(csv_dir + name + '.csv', delimiter='\n'))
+    scaler =StandardScaler()
+    scaler.fit(files)
+    return scaler
+
 
 def get_batch(batch_size=10):
     return torch.randn(batch_size,3, 450, 450),torch.randn(batch_size,199)
 
 if __name__ == '__main__':
     batch = get_batch(10)
-
-
+    scaler = prepare_scaler('train.csv', './train/')
+    scaler.transform(batch)
     module = models.ResNetDepth(num_channels=3,layers=[1,1,1,1],num_elements=160)
     with torch.no_grad():
         module, batch = module.cuda(), batch.cuda()
